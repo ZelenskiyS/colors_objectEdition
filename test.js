eca3272f0f471414a2bbe5579c759c5f37ff0b24
ZelenskiyS <54572044+ZelenskiyS@users.noreply.github.com>
2020-07-13 11:23:30 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,20 @@
-import {colors} from "./index.js";
-
-console.log(colors.stylize(colors.stylize('TEST ME','red'), 'bgCyan'));
-console.log(colors.trap('TEST ME'));
-console.log(colors.zebra('TEST ME'));
-console.log(colors.random('TEST ME'));
-console.log(colors.rainbow('TEST ME'));
-console.log(colors.america('TEST ME'));
+import {colors} from "./index.js";                 
+                                                   
+                                                   
+console.log(colors.trap('TEST ME'));               
+console.log(colors.zebra('TEST ME'));              
+console.log(colors.random('TEST ME'));             
+console.log(colors.rainbow('TEST ME'));            
+console.log(colors.america('TEST ME'));            
+                                                   
+                                                   
+                                                   
+console.log(colors.error('ERROR Test'));           
+console.log(colors.warn('Warning'));               
+console.log(colors.input('test'));                 
+console.log(colors.debug('Debugging test'));       
+                                                   
+colors.setTheme.custom=['whiteBG','cyan', 'underline'];
+                                                   
+console.log(colors.custom('Custom test'));         
+                                                   
