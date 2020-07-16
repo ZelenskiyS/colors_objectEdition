e7e1ce9726e8cfb0b52b58ad7f3553f20ecfe84e
ZelenskiyS <54572044+ZelenskiyS@users.noreply.github.com>
2020-07-16 10:27:38 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,20 +1,16 @@
-import {colors} from "./index.js";                 
-                                                   
-                                                   
-console.log(colors.trap('TEST ME'));               
-console.log(colors.zebra('TEST ME'));              
-console.log(colors.random('TEST ME'));             
-console.log(colors.rainbow('TEST ME'));            
-console.log(colors.america('TEST ME'));            
-                                                   
-                                                   
-                                                   
-console.log(colors.error('ERROR Test'));           
-console.log(colors.warn('Warning'));               
-console.log(colors.input('test'));                 
-console.log(colors.debug('Debugging test'));       
-                                                   
-colors.setTheme.custom=['whiteBG','cyan', 'underline'];
-                                                   
-console.log(colors.custom('Custom test'));         
-                                                   
+import {colors} from "./index.js";                                  
+                                                                    
+console.log(colors.stylize('Color test', 'brightYellow'))           
+                                                                    
+console.log(colors.trap('TRAP TEST'));                              
+console.log(colors.zebra('ZEBRA test'));                            
+console.log(colors.random('Random test'));                          
+console.log(colors.rainbow('Rainbow test'));                        
+console.log(colors.america('America test'));                        
+                                                                    
+console.log(colors.theme('Error theme test','error'));              
+console.log(colors.theme('Warning theme test','warn'));             
+                                                                    
+colors.setTheme.testTheme=['brightCyan','italic','magentaBG'];      
+                                                                    
+console.log(colors.theme('User theme test', 'testTheme'));          
